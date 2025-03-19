7ab60263c841b7b056d51bc7c995f15a1837b0a2
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2025-03-19 09:08:32 -0300

    crt

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,12 +18,7 @@ SUPABASE_URL = os.getenv("VITE_SUPABASE_URL")
 SUPABASE_KEY = os.getenv("VITE_SUPABASE_ANON_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-ssl_cert_path = os.getenv("SSL_CERT_PATH")
 db_url = os.getenv("VITE_SUPABASE_DB")
-
-# ✅ Garante que SSL é aplicado corretamente na string de conexão
-if db_url and "?sslmode=" not in db_url:
-    db_url += f"?sslmode=verify-ca&sslrootcert={ssl_cert_path}"
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
