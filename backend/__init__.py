b495b30b2a74ffb7fc49a0cb1b4ba48a06c0573b
Carlos magno <83795938+Magno-programmer@users.noreply.github.com>
2025-03-19 09:35:06 -0300

    teste certificado denovo

--- backend/__init__.py
+++ backend/__init__.py
@@ -24,9 +24,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 # Define o caminho onde o certificado será salvo temporariamente
 ssl_cert_path = "/tmp/supabase.crt"
+supabase_cert_content = os.getenv("SUPABASE_CERT")
 
 # Obtém o certificado da variável de ambiente e escreve no arquivo
-supabase_cert_content = os.getenv("SUPABASE_CERT")
 if supabase_cert_content:
     with open(ssl_cert_path, "w") as cert_file:
         cert_file.write(supabase_cert_content)
